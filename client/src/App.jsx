e54711509c316726bf5db0dd29a92d9ece888616
ald2211 <afnadca2@gmail.com>
2024-06-18 01:57:52 +0530

    >installed react.js and integrated tailwind css.                                 >server created and verified the successfull operation

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-700 text-center'>hello,how are you</h1>
+    </div>
+  )
+}
+
+export default App
