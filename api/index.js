6021826dcbf600f99c56cfbeeada469787bead71
ald2211 <afnadca2@gmail.com>
2024-06-20 20:52:20 +0530

    complete database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('db connected succefully')
+}).catch((err)=>console.log(err))
 const app=express()
 
 app.get('/',(req,res)=>{
