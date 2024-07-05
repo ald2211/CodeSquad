0fe76dc273c10e507022dff8943abf64d0d405d9
ald2211 <afnadca2@gmail.com>
2024-07-06 04:48:25 +0530

    >small correction at user route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -10,21 +10,21 @@ const router = express.Router();
 router.patch("/upload/:id",verifyUser(['developer','client']),updateUserProfile);
 
 //education section
-router.post("/education/add/:id", verifyUser([['developer','client']]), addEducation);
-router.patch("/education/edit/:edu_id/:user_id", verifyUser([['developer','client']]), editEducation);
-router.delete("/education/delete/:id", verifyUser([['developer','client']]), deleteEducation);
-router.get("/education/get", verifyUser([['developer','client']]), getEducation);
+router.post("/education/add/:id", verifyUser(['developer','client']), addEducation);
+router.patch("/education/edit/:edu_id/:user_id", verifyUser(['developer','client']), editEducation);
+router.delete("/education/delete/:id", verifyUser(['developer','client']), deleteEducation);
+router.get("/education/get", verifyUser(['developer','client']), getEducation);
 
 //experience section
-router.post("/experience/add/:id", verifyUser([['developer','client']]), addExperience);
-router.patch("/experience/edit/:exp_id/:user_id", verifyUser([['developer','client']]), editExperience);
-router.delete("/experience/delete/:id", verifyUser([['developer','client']]), deleteExperience);
-router.get("/experience/get", verifyUser([['developer','client']]), getExperience);
+router.post("/experience/add/:id", verifyUser(['developer','client']), addExperience);
+router.patch("/experience/edit/:exp_id/:user_id", verifyUser(['developer','client']), editExperience);
+router.delete("/experience/delete/:id", verifyUser(['developer','client']), deleteExperience);
+router.get("/experience/get", verifyUser(['developer','client']), getExperience);
 
 //projects section
-router.post("/projects/add/:id", verifyUser([['developer','client']]), addProjects);
-router.patch("/projects/edit/:proj_id/:user_id", verifyUser([['developer','client']]), editProjects);
-router.delete("/projects/delete/:id", verifyUser([['developer','client']]), deleteProjects);
-router.get("/projects/get", verifyUser([['developer','client']]), getProjects);
+router.post("/projects/add/:id", verifyUser(['developer','client']), addProjects);
+router.patch("/projects/edit/:proj_id/:user_id", verifyUser(['developer','client']), editProjects);
+router.delete("/projects/delete/:id", verifyUser(['developer','client']), deleteProjects);
+router.get("/projects/get", verifyUser(['developer','client']), getProjects);
 
 export default router;
